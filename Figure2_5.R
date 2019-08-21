bbee54c26cb81921b3b886ce89c2c13bcd9f84cd
Neil Burger <cnburger13@gmail.com>
2019-08-21 23:26:50 +0200

    Code for Figure 2.5 ESLT

---
+++ Figure2_5.R
@@ -0,0 +1,22 @@
+library(ElemStatLearn)
+#Using the elements of statistical learning package provided in the book
+str(mixture.example)
+
+#Extracting the probabilities
+probs<-mixture.example$prob 
+
+#Extracting probability of x1 & x2 values
+px1 <- mixture.example$px1 
+px2 <- mixture.example$px2
+
+#Extractig x1 & x2 values
+x <- mixture.example$x
+
+#crating a matrix of probabilities
+probabilites.inmat <- matrix(probs, length(px1), length(px2)) 
+
+#Adding the decision boundary
+contour(px1, px2, probabilites.inmat, 
+        levels=0.5, labels="", xlab="x1", ylab="x2", main="Bayes boundary") 
+points(x, col=ifelse(g==1, "red", "blue"))
+
