ab64c3d0f6747deb1935abaf1666d9d3abc937d1
Neil Burger <cnburger13@gmail.com>
2019-08-22 09:10:23 +0200

    Fixed
    Error in plotting datapoints

--- Figure2_5.R
+++ Figure2_5.R
@@ -1,3 +1,4 @@
+install.packages('ElemStatLearn')
 library(ElemStatLearn)
 #Using the elements of statistical learning package provided in the book
 str(mixture.example)
@@ -12,11 +13,12 @@ px2 <- mixture.example$px2
 #Extractig x1 & x2 values
 x <- mixture.example$x
 
+
 #crating a matrix of probabilities
 probabilites.inmat <- matrix(probs, length(px1), length(px2)) 
 
 #Adding the decision boundary
 contour(px1, px2, probabilites.inmat, 
         levels=0.5, labels="", xlab="x1", ylab="x2", main="Bayes boundary") 
-points(x, col=ifelse(g==1, "red", "blue"))
+points(x, col=ifelse(mixture.example$y==1, "red", "blue"))
 
